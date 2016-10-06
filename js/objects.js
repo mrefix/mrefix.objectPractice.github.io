5c655a418b66295f6985d5c4e779c562d3222cae
Danny Capaccio <capaccdp@miamioh.edu>
2016-10-06 11:19:36 -0400

    End of Class Thursday

--- js/objects.js
+++ js/objects.js
@@ -1,13 +1,33 @@
 var centerX;
 var centerY;
+var b2;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
     //initialize variables
     centerX = windowWidth / 2;
     centerY = windowHeight / 2;
+    b2 = new bob();
+    b2.x = 40;
 }
 
 function draw() {
     ellipse(centerX, centerY, 50, 50);
+    var b1 = new bob();
+    b1.display();
+    b2.moveBob();
+    b2.display();
+}
+
+function bob() {
+
+    this.x = 200;
+    this.y = 100;
+    this.moveBob = function() {
+        this.x++;
+    }
+    this.display = function() {
+        ellipse(this.x, this.y, 60, 60);
+    }
+
 }
